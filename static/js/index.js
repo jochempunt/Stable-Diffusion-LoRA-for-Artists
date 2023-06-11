dd5b1905af872c772cf3f791c7578a59809f5ed2
jochempunt <80535619+jochempunt@users.noreply.github.com>
2023-06-11 22:53:17 +0200

    added some functionality

--- static/js/index.js
+++ static/js/index.js
@@ -5,17 +5,18 @@ $(document).ready(function() {
     // Check for click events on the navbar burger icon
 
     var options = {
-			slidesToScroll: 1,
-			slidesToShow: 1,
-			loop: true,
-			infinite: true,
-			autoplay: true,
-			autoplaySpeed: 5000,
+        slidesToScroll: 1,
+        slidesToShow: 1,
+        loop: true,
+        infinite: true,
+        autoplay: true,
+        autoplaySpeed: 5000,
     }
 
-		// Initialize all div with carousel class
+    // Initialize all div with carousel class
     var carousels = bulmaCarousel.attach('.carousel', options);
-	
+
     bulmaSlider.attach();
+    initComparisons();
 
 })
